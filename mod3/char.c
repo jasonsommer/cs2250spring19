3d0a5604102560a243c8730f85e3b49f109c1f77
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-01-22 09:04:15 -0700

    lol

---
+++ mod3/char.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  char.c
+ *
+ *    Description:  Learn how to use characters
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:53:44 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weigt = 178.3;
+    char sex = 'M';
+
+    printf("YOur age is %d and your weight %lf and your sex is %c\n",
+            age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+
