7932c78463556543217899a2bfce4cad6697ce53
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-01-24 09:42:20 -0700

    add even_odd

---
+++ mod3/even_odd.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  even_odd.c
+ *
+ *    Description:  Determine if the input value is even or odd
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 09:37:16 AM
+ *       Revision:  none
+ *       Compiler:  gcc even_odd.c -o even_odd.out
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int inputNum = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &inputNum);
+    //test value for even or odd
+    if(inputNum % 2 == 0)
+    {
+      printf("%d is even\n", num;
+    }
+
+    return 0;
+}
+// Function Definitions
+
+
