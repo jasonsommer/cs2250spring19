c7363c4f010f1cedb6fcd43221f76320c6cd9f4c
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-01-31 09:59:46 -0700

    finished month_day.c

--- mod4/month_day.c
+++ mod4/month_day.c
@@ -17,6 +17,19 @@
  */
 #include <stdio.h>
 
+
+#define JANUARY     1
+#define FEBRUARY    2
+#define MARCH       3
+#define APRIL       4
+#define MAY         5
+#define JUNE        6
+#define JULY        7
+#define AUGUST      8
+#define SEPTEMBER   9
+#define OCTOBER     10
+#define NOVEMBER    11
+#define DECEMBER    12
 // Constants
 
 // Function Prototypes
@@ -24,13 +37,95 @@
 // Main Function
 int main()
 {
+int month = 0;
+int day = 0;
+int year = 0;
+int monthVer =0;
+int dayVer = 0;
+char answer = 'y';
 
+while(1)
+{
     //Capture user input
-    //
+    printf("I will tell you if your date is correct. What is the date? (mm/dd/yyyy) ");
+    scanf("%d/%d/%d", &month, &day, &year);
     //Use if to validate month
-    //
-    //Use SWITCH to validate day of the month
+    if(month>0 && month<13)
+    {
+        monthVer =1;
+    }
+    else 
+    {
+        monthVer =0;
+    }
+    if( year%4==0 && month ==2)
+    {
+        month=13;
+    }
 
+    //Use SWITCH to validate day of the month
+    switch(month)
+    {
+        //Months with 31 days
+        case JANUARY:
+        case MARCH:
+        case MAY:
+        case JULY:
+        case AUGUST:
+        case OCTOBER:
+        case DECEMBER:
+            if(day>0 && day<32)
+            {
+                dayVer =1;
+            }
+
+            break;
+            //Months with 30 days
+        case APRIL:
+        case JUNE:
+        case SEPTEMBER:
+        case NOVEMBER:
+            if (day>0 && day<31)
+            {
+                dayVer =1;
+            }
+            break;
+            //For febuary
+        case FEBRUARY:
+            if((day>0 && day<29)||((year%4==0)&&(day>0 && day<30)))
+            {
+                dayVer =1;
+            }
+
+            break;      
+            //For leap year
+    }
+
+
+
+    if(monthVer==1 && dayVer ==1)
+    {
+        printf("Your date is valid\n");
+    }
+    else
+    {
+        printf("Invalid date\n");
+    }
+
+    monthVer= 0;
+    dayVer=0;
+
+    printf("do you want to continue?(y/n)\n");
+    scanf(" %c", &answer);
+
+    if (answer != 'y' && answer != 'Y')
+    {
+        break; //exit loop
+    }
+
+
+}
+printf("\nPeace out sucker\n\n");
     return 0;
 }
 // Function Definitions
