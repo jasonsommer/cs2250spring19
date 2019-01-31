222fa38f52d14e5d016da936fb62dff68254ff29
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-01-31 09:19:30 -0700

    started month_day

---
+++ mod4/month_day.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:18:07 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    //Capture user input
+    //
+    //Use if to validate month
+    //
+    //Use SWITCH to validate day of the month
+
+    return 0;
+}
+// Function Definitions
+
+
