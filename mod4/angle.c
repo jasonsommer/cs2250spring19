0846b3eaf810ba4bd03bf503defeb7701847a466
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-01-31 08:38:59 -0700

    add angle.c

---
+++ mod4/angle.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  angle.c
+ *
+ *    Description:  Calculate the quadrant based on input angle.
+ *                  Quadrant I: angle between 0-90
+ *                  Quadrant II: angle between 90-180
+ *                  Quadrant III: angle between 180-270
+ *                  Quadrant IV: angle between 270-360
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 08:35:12 AM
+ *       Revision:  none
+ *       Compiler:  gcc angle.c -o angle.out
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int userAngle=0;
+
+    printf("
+    return 0;
+}
+// Function Definitions
+
+
