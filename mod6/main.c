33cb2273903ba7f17dbd61c976769bd8cea9527f
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-02-19 22:34:59 -0700

    finished stuff

--- mod6/main.c
+++ mod6/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-
+#define NUMP 5
 int main(void) {
+int jersey[NUMP];
+int rating[NUMP];
 
+for(int i=0; i<NUMP; i++)
+{
+    printf("Enter player %d's jersey number:\n", i+1);
+    scanf("%d", &jersey[i]);
+    printf("Enter player %d's rating:\n", i+1);
+    scanf("%d", &rating[i]);
+}
+printf("ROSTER\n");
+for(int i=0; i<NUMP; i++)
+{
+    printf("Player %d -- Jersey number: %d, Rating: %d\n", i+1, jersey[i], rating[i]);
+}
    /* Type your code here. */
 
    return 0;
