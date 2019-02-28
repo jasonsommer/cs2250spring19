3ba812fbf46959f9d468ae1e3ba03971719f256c
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-02-28 10:12:17 -0700

    updated

---
+++ mod8/calculator.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  calculator.c
+ *
+ *    Description:  Calculator libraryfunction implementation file
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:56:14 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *          Usage:  #include "calculator.h"
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculator.h"
+
+int Add_Numbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+int Sub_Numbers(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+// Function Definitions
+
+
---
+++ mod8/calculator.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  calculator.h
+ *
+ *    Description: calculator library 
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:56:14 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jason Sommer (), Jasonsommer@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  CALCULATOR__INC__
+#define  CALCULATOR__INC__
+// constants
+//
+// Prototypes
+int Add_Numbers(int num1, int num2);
+int Sub_Numbers(int num1, int num2);
+
+#endif /* ----- #ifndef CALCULATOR__INC__ ----- */
+
