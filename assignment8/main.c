9f88739cebbba1b08accfbc44b48d8f3cb1c6efc
Jason Sommer <Jasonsommer@mail.weber.edu>
2019-03-12 18:12:57 -0600

    added main.c

--- assignment8/main.c
+++ assignment8/main.c
@@ -27,7 +27,8 @@ const int STRLEN = 1000; //may need to be increase depending on
 // Constants
 
 // Function Prototypes
-
+char PrintMenu();
+void Menu();
 // Main Function
 int main(int argc, char* argv[])
 {
@@ -35,10 +36,39 @@ int main(int argc, char* argv[])
     char userInput[STRLEN];
     printf("Enter a sample text:\n");
     fgets(userInput, STRLEN, stdin);
-    printf("\n\n%s\n", userInput);
+    printf("\nYou entered: %s", userInput);
 
     return 0;
 }
 // Function Definitions
+//
+void Menu()
+{
+    printf("MENU\nc - Number of non-whitespace characters\n");
+    printf("w - Number of words\nf - Fix capitalizaton\n");
+    printf("r - Replace all !'s\ns - Shorten spaces\nq - Quit\n\n");
+    printf("Choose an option:");
+    PrintMenu();
+    
+    return;
+}
+
+char PrintMenu()
+{
+    char userChoice;
+    int temp=0;
+    Menu();
+    while(temp==0)
+    {
+    scanf("%c", &userChoice);
+    if((userChoice=='c')||(userChoice=='w')||(userChoice=='f')||(userChoice=='r')||(userChoice=='s')||(userChoice=='q'))
+    {
+        temp++;
+    }
+    }
+    
+return userChoice;
+
+}
 
 
